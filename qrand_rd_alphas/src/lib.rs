7c88b6acac08deb100352f6dc2b546f8311ba409
rusty-octopus <rustyoctopus@posteo.org>
2020-09-14 21:09:25 +0200

    qrand_core_alphas is not a procedural macro anymore. Set version number to 0.3.0

--- qrand_rd_alphas/src/lib.rs
+++ qrand_rd_alphas/src/lib.rs
@@ -1,46 +1,20 @@
-use proc_macro::TokenStream;
-
-fn calculate_phi(dimension: u32) -> f64 {
-    let mut x: f64 = 2.0;
-    let power: f64 = f64::from(dimension + 1).recip();
-    for i in 0..25 {
-        x = x + 1.0;
-        x = x.powf(power);
+fn calculate_phi(dimension: usize) -> f64 {
+    let mut phi: f64 = 2.0;
+    let power: f64 = f64::from(dimension as u32 + 1).recip();
+    for _ in 0..25 {
+        phi += 1.0;
+        phi = phi.powf(power);
     }
-    x
+    phi
 }
 
-fn create_alphas(dimension: u32) -> Vec<f64> {
-    let mut vec: Vec<f64> = Vec::with_capacity(dimension as usize);
+pub fn create(dimension: usize) -> Vec<f64> {
+    let mut alphas: Vec<f64> = Vec::with_capacity(dimension);
     let phi = calculate_phi(dimension);
     let inv_g = phi.recip();
-    let dim = dimension as usize;
-    for i in 0..dimension {
-        vec.push(inv_g.powf(f64::from(i + 1)).fract());
+    let dim = dimension as u32;
+    for i in 0..dim {
+        alphas.push(inv_g.powf(f64::from(i + 1)).fract());
     }
-    vec
-}
-
-#[proc_macro]
-pub fn create(dimension: TokenStream) -> TokenStream {
-    // Use https://doc.rust-lang.org/std/macro.compile_error.html for error reporting?
-    println!("Dimension {}", dimension);
-    // let dim_string = dimension.to_string();
-    // let dim = dim_string.parse::<u32>().unwrap();
-    //let input = parse_macro_input!(dimension as DeriveInput);
-    //println!("syn input: {:?}", input);
-    let dim = 2;
-    let alphas = create_alphas(dim);
-    let mut code = String::from("const alphas:[f64;");
-    code.push_str(dim.to_string().as_str());
-    code.push_str("] = [");
-    for i in 0..dim - 1 {
-        code.push_str(alphas[i as usize].to_string().as_str());
-        code.push_str(", ");
-    }
-    code.push_str(alphas.last().unwrap().to_string().as_str());
-    code.push_str("];");
-
-    println!("{}", code);
-    code.parse().unwrap()
+    alphas
 }
--- qrand_rd_alphas/tests/integration_tests.rs
+++ qrand_rd_alphas/tests/integration_tests.rs
@@ -1,13 +1,11 @@
 #[cfg(test)]
 #[cfg_attr(tarpaulin, skip)]
 mod integration_tests {
-    use rd_alphas::create;
-
-    const DIMENSION: usize = 5;
-    create!(2);
+    use qrand_rd_alphas::create;
 
     #[test]
     fn test_create_alphas() {
+        let alphas = create(2);
         assert_eq!(0.7548776662466927, alphas[0]);
         assert_eq!(0.5698402909980532, alphas[1]);
     }
