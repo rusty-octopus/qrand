e0fe41d489ba6a25b4875391bd5b108f5a81ea30
rusty-octopus <rustyoctopus@posteo.org>
2020-10-12 22:46:50 +0200

    Re-executed code coverage with 'deactived' workspace.

--- qrand_std/tests/integration_tests.rs
+++ qrand_std/tests/integration_tests.rs
@@ -4,7 +4,6 @@ mod qrand_std_integration_tests {
     use qrand_std::create_sequence;
     use qrand_std::LowDiscrepancySequence;
 
-    #[cfg(all(feature = "rd", feature = "std_interface"))]
     #[test]
     fn test_rd_sequence_std() {
         let sequence = create_sequence(2);
@@ -14,5 +13,15 @@ mod qrand_std_integration_tests {
         assert_eq!(0.5698402909980532, sequence.element(1, 1).unwrap_or(1.1));
         assert_eq!(0.5097553324933854, sequence.element(2, 0).unwrap_or(1.1));
         assert_eq!(0.13968058199610645, sequence.element(2, 1).unwrap_or(1.1));
+    }
+
+    #[test]
+    fn test_error() {
+        let sequence = create_sequence(2);
+        assert_eq!(Err(qrand_core::QrandCoreError::PointElementNotExisting("Point element not existing. Element of point is too large for the chosen dimension.")), sequence.element(0, 5));
+        assert_eq!(
+            "Point element not existing. Element of point is too large for the chosen dimension.",
+            format!("{}", sequence.element(0, 5).unwrap_err())
+        );
     }
 }
